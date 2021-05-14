ced4e66d43c1ba9b58f0a4db2a4b7da10a40df9f
Jon Kim <yongrou74@hotmail.com>
2021-05-14 23:22:08 +0800

    slider backgroud

--- app/Views/templateone/index.php
+++ app/Views/templateone/index.php
@@ -4,35 +4,25 @@
         <section class="slider-area ">
             <div class="slider-active">
                 <!-- Single Slider -->
-                <div class="single-slider slider-bg1 slider-height d-flex align-items-center">
+                <?php foreach($banner as $row){ ?>
+                <div class="single-slider slider-height d-flex align-items-center" style="background-image: url(<?= base_url() . $row['banner'] ?>);">
                     <div class="container">
                         <div class="rowr">
                             <div class="col-xxl-5 col-xl-6 col-lg-7 col-md-8  col-sm-10">
                                 <div class="hero-caption text-center">
                                     <!-- <span>Fashion Sale</span> -->
-                                    <h1 data-animation="bounceIn" data-delay="0.2s">Minimal Menz Style</h1>
-                                    <p data-animation="fadeInUp" data-delay="0.4s">Consectetur adipisicing elit. Laborum fuga incidunt laboriosam voluptas iure, delectus dignissimos facilis neque nulla earum.</p>
+                                    <h1 data-animation="bounceIn" data-delay="0.2s"><?= $row['title'] ?></h1>
+                                    <p data-animation="fadeInUp" data-delay="0.4s"><?= $row['description'] ?></p>
                                     <a href="#" class="btn_1 hero-btn" data-animation="fadeInUp" data-delay="0.7s">Shop Now</a>
                                 </div>
                             </div>
                         </div>
                     </div>          
                 </div>
+                <?php } ?>
+
                 <!-- Single Slider -->
-                <div class="single-slider slider-bg2 slider-height d-flex align-items-center">
-                    <div class="container">
-                        <div class="row justify-content-end">
-                            <div class="col-xxl-5 col-xl-6 col-lg-7 col-md-8 col-sm-10">
-                                <div class="hero-caption text-center">
-                                    <!-- <span>Fashion Sale</span> -->
-                                    <h1 data-animation="bounceIn" data-delay="0.2s">Minimal Menz Style</h1>
-                                    <p data-animation="fadeInUp" data-delay="0.4s">Consectetur adipisicing elit. Laborum fuga incidunt laboriosam voluptas iure, delectus dignissimos facilis neque nulla earum.</p>
-                                    <a href="#" class="btn_1 hero-btn" data-animation="fadeInUp" data-delay="0.7s">Shop Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>          
-                </div>
+                
             </div>
         </section>
         <!-- slider Area End-->
