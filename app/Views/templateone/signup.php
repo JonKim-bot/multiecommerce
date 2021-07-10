8c7c1469181c5302d4c96a104c3524ca60c02796
Jon Kim <yongrou74@hotmail.com>
2021-07-10 18:10:05 +0800

    form

--- app/Views/templateone/signup.php
+++ app/Views/templateone/signup.php
@@ -1,51 +1,61 @@
-
-        <!-- Register Area Start -->
-        <div class="register-form-area">
-            <div class="register-form text-center">
-                <!-- Login Heading -->
-                <div class="register-heading">
-                    <span>Sign Up</span>
-                    <p>Create your account to get full access</p>
-                </div>
-                <!-- Single Input Fields -->
-                <form role="form" method="POST" enctype="multipart/form-data" action="<?= base_url() ?>/main/signup">
-
-                <div class="input-box">
-                     <?php if (isset($error)) { ?>
-                        <div class="alert-message text-danger" role="alert">
-                            <h2 class="text-danger">* <?= $error; ?></h2>
-                        </div>
-                    <?php } ?>
-                    <div class="single-input-fields">
-                        <label>Full name</label>
-                        <input type="text" name="name" placeholder="Enter full name" required>
-                    </div>
-                    <div class="single-input-fields">
-                        <label>Email Address</label>
-                        <input type="email" name="email" placeholder="Enter email address" required>
-                    </div>
-                    <div class="single-input-fields">
-                        <label>Contact</label>
-                        <input type="tel" name="contact" placeholder="Enter contact" required>
-                    </div>
-                    <?php if(in_array(6,$shop_function)){ ?>
-                        <div class="single-input-fields">
-                            <label>Referal Code</label>
-                            <input type="text" name="referal_code" value="<?= $referal_code ?>" placeholder="Optional">
-                        </div>
-                    <?php } ?>
-                    <div class="single-input-fields">
-                        <label>Password</label>
-                        <input type="password" name="password" placeholder="Enter Password" required>
-                    </div>
-          
-                </div>
-                <!-- form Footer -->
-                <div class="register-footer">
-                    <p> Already have an account? <a href="<?= base_url()  ?>/main/login"> Login</a> here</p>
-                    <button class="submit-btn3" type="submit">Sign Up</button>
-                </div>
-                </form>
-            </div>
+<!-- login Area Start -->
+<div class="c-login-back">
+    </div>
+    <div class="login-form-area c-login-form">
+        <div class="login-form">
+        <div class="c-header text-center">
+            <h1>Sign Up</h1>
         </div>
-        <!-- Register Area End -->
+        <!-- Login Heading -->
+        <!-- <div class="login-heading">
+            <span>Login</span>
+            <p>Login now to get voucher benefit ! </p>
+        </div> -->
+        <!-- Single Input Fields -->
+        <form role="form" method="POST" enctype="multipart/form-data" action="<?= base_url() ?>/main/signup">
+
+            <div class="input-box">
+                <?php if (isset($error)) { ?>
+                    <div class="alert-message text-danger" role="alert">
+                        <h2 class="text-danger">* <?= $error; ?></h2>
+                    </div>
+                <?php } ?>
+                <div class="single-input-fields">
+                    <label class="c-label">Full name</label>
+                    <input type="text" class="c-input" name="name" placeholder="Enter full name" required>
+                </div>
+                <div class="single-input-fields">
+                    <label class="c-label">Email Address</label>
+                    <input  class="c-input" type="email" name="email" placeholder="Enter email address" required>
+                </div>
+                <div class="single-input-fields">
+                    <label class="c-label">Contact</label>
+                    <input  class="c-input" type="tel" name="contact" placeholder="Enter contact" required>
+                </div>
+                <?php if(in_array(6,$shop_function)){ ?>
+                    <div class="single-input-fields">
+                        <label class="c-label">Referal Code</label>
+                        <input  class="c-input" type="text" name="referal_code" value="<?= $referal_code ?>" placeholder="Optional">
+                    </div>
+                <?php } ?>
+                <div class="single-input-fields">
+                    <label class="c-label">Password</label>
+                    <input  class="c-input" type="password" name="password" placeholder="Enter Password" required>
+                </div>
+                <!-- <div class="single-input-fields login-check">
+                            <input type="checkbox" id="fruit1" name="keep-log">
+                            <label for="fruit1">Keep me logged in</label>
+                        <a href="#" class="f-right">Forgot Password?</a>
+                        </div> -->
+            </div>
+
+            <!-- form Footer -->
+            <div class="login-footer">
+            <p> Already have an account? <a href="<?= base_url()  ?>/main/login"> Login</a> here</p>
+                <button class="submit-btn3 c-btn" type="submit">Sign Up</button>
+            </div>
+        </form>
+
+    </div>
+</div>
+<!-- login Area End -->
