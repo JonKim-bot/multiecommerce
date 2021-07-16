79b0e7921c85e65c3109795850f2fd34dc5bd667
sakuralun96 <sakuralun96@hotmail.com>
2021-07-17 03:34:22 +0800

    homepage

--- app/Views/templateone/product_list.php
+++ app/Views/templateone/product_list.php
@@ -3,33 +3,35 @@
         <input type="hidden" id="max_product_price" value="<?= $product_max_price ?>">
         <?php foreach ($product as $row) { ?>
             <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 d-flex">
-                <div class="c-Cardflex">
-                    <div class="c-listingCard">
-                        <div class="c-top">
-                            <div class="c-tag">最新!</div>
-                            <div class="c-productImg">
-                                <img src="<?= base_url() .  $row['image'] ?>" alt="">
+                <a href="<?= base_url() . "/main/product_detail/" .   $row['product_id'] ?>">
+                    <div class="c-Cardflex">
+                        <div class="c-listingCard">
+                            <div class="c-top">
+                                <div class="c-tag">最新!</div>
+                                <div class="c-productImg">
+                                    <img src="<?= base_url() .  $row['image'] ?>" alt="">
+                                </div>
+                                <div class="c-name">
+                                    <h3><?= $row['product_name'] ?></h3>
+                                </div>
+                                <div class="c-desc">
+                                    <p><?= $row['product_description'] ?></p>
+                                </div>
                             </div>
-                            <div class="c-name">
-                                <h3><?= $row['product_name'] ?></h3>
-                            </div>
-                            <div class="c-desc">
-                                <p><?= $row['product_description'] ?></p>
-                            </div>
-                        </div>
-                        <div class="c-btm">
-                            <div class="c-btn">了解更多</div>
-                            <div class="c-price">
-                                <?php if ($row['is_promo'] == 1) { ?>
-                                    <p class="t-promo-price">RM <?= $row['product_price'] ?></p>
-                                    <p class="t-price">RM <?= $row['promo_price'] ?></p>
-                                <?php } else { ?>
-                                    <p class="t-price">RM <?= $row['product_price'] ?></p>
-                                <?php } ?>
+                            <div class="c-btm">
+                                <div class="c-btn">了解更多</div>
+                                <div class="c-price">
+                                    <?php if ($row['is_promo'] == 1) { ?>
+                                        <p class="t-promo-price">RM <?= $row['product_price'] ?></p>
+                                        <p class="t-price">RM <?= $row['promo_price'] ?></p>
+                                    <?php } else { ?>
+                                        <p class="t-price">RM <?= $row['product_price'] ?></p>
+                                    <?php } ?>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
         <?php } ?>
     </div>
